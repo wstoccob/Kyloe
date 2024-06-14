e873765fda3d554202c18cb59dba886547287e68
wstoccob <alinur.seysekov@gmail.com>
2024-06-15 01:07:50 +0500

    Add UserViewModel

---
+++ Kyloe/ViewModels/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Kyloe.ViewModels;
+
+public class UserViewModel
+{
+    public int UserId { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public IList<string> Roles { get; set; }
+}
